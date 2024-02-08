654f16a78315557d2da2bfe337c0f163aa29fab0
matteopiatti <github@piatti.li>
2024-02-08 16:06:13 +0100

    Location allow

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 import './style.css'
 import { store } from './scripts/store.js'
+import { utils } from './scripts/utils.js'
+import { timer } from './scripts/timer.js'
 
 // Achievement 1
 store.startBtn.addEventListener('click', function startListener () {
   store.start()
   store.startBtn.removeEventListener('click', startListener)
   store.startBtn.addEventListener('click', function stopListener () {
+    console.log('asdf')
     store.stop()
     store.startBtn.removeEventListener('click', stopListener)
   })
 })
+
+// Achievement 2
+const locate = async () => {
+  await utils.until(_ => timer.actualTime >= 8)
+  navigator.geolocation.getCurrentPosition(
+    () => {
+      store.achievements.set(2)
+    },
+    () => {
+    })
+}
+locate()
